afb182e1a2b8a1017f9655183bf5a6355ea8c9b5
Yathartha <varshneya.yathartha@gmail.com>
2023-10-13 19:37:52 +0530

    Added user controller

--- controllers/user.py
+++ controllers/user.py
@@ -1,10 +1,52 @@
-from fastapi import APIRouter
+from typing import List, Optional
+from fastapi import APIRouter, Body, Path, Query
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/user',
     tags=['user']
 )
 
-@router.post('/new', summary='Create a user')
-def create_user():
-  pass
+class BlogModel(BaseModel):
+  title: str
+  content: str
+  nb_comments: int
+  published: Optional[bool]
+
+@router.post('/', response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+  return db_user.create_user(db, request)
+
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel, id: int, 
+        comment_title: int = Query(None,
+            title='Title of the comment',
+            description='Some description for comment_title',
+            alias='commentTitle',
+            deprecated=True
+        ),
+        content: str = Body(...,    # .../Ellipsis means the value is required for this param
+            min_length=10,
+            max_length=50,
+            regex='^[a-z]*$'
+        ),
+        v: Optional[List[str]] = Query(['1.0', '1.1', '1.2']),
+        comment_id: int = Path(le=5)
+    ):
+    return {
+        'blog': blog,
+        'id': id,
+        'comment_title': comment_title,
+        'content': content,
+        'version': v,
+        'comment_id': comment_id
+    }
+
+@router.post('/new/{id}')
+def create_blog(blog: BaseModel, id: int, version: int = 1):
+  return {
+    'id': id,
+    'data': blog,
+    'version': version
+    }
+ 
